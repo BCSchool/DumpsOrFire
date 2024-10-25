dd51da2f22381388a5c927ef308f2cfae86a36e9
josh <joshuaholman5@gmail.com>
2024-10-24 18:58:29 -0700

    added event listener

---
+++ DumpsOrFire/spotify/static/spotify/fieldText.js
@@ -0,0 +1,19 @@
+const searchTypeDropdown = document.getElementById("search_type_dropdown");
+const searchInput = document.getElementById("search_input");
+
+const placeholderTexts = {
+  track: 'Search for a song...',
+  album: 'Search for an album...',
+  playlist: 'Search for a playlist...',
+  link: 'Paste a Spotify link...'
+};
+
+searchTypeDropdown.addEventListener('change', function() {
+  const selectedType = this.value;
+  searchInput.placeholder = placeholderTexts[selectedType];
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const initialType = searchTypeDropdown.value;
+  searchInput.placeholder = placeholderTexts[initialType];
+})
