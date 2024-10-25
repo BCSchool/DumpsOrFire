adbce85562b627895a094c437264985e0917fa32
josh <joshuaholman5@gmail.com>
2024-10-24 19:34:11 -0700

    formatting

--- DumpsOrFire/spotify/static/spotify/fieldText.js
+++ DumpsOrFire/spotify/static/spotify/fieldText.js
@@ -2,18 +2,18 @@ const searchTypeDropdown = document.getElementById("search_type_dropdown");
 const searchInput = document.getElementById("search_input");
 
 const placeholderTexts = {
-  track: 'Search for a song...',
-  album: 'Search for an album...',
-  playlist: 'Search for a playlist...',
-  link: 'Paste a Spotify link...'
+  track: "Search for a song...",
+  album: "Search for an album...",
+  playlist: "Search for a playlist...",
+  link: "Paste a Spotify link...",
 };
 
-searchTypeDropdown.addEventListener('change', function() {
+searchTypeDropdown.addEventListener("change", function () {
   const selectedType = this.value;
   searchInput.placeholder = placeholderTexts[selectedType];
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   const initialType = searchTypeDropdown.value;
   searchInput.placeholder = placeholderTexts[initialType];
-})
+});
